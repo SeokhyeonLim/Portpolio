c5ecf4bf05743816f2e9077cd5bc8fa8a1b55756
SeokhyeonLim <112453492+SeokhyeonLim@users.noreply.github.com>
2023-12-25 00:44:43 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Upper from './Main/Upper';
+
+function App() {
+  return (
+    <div className="App">
+      <Upper/>
+    </div>
+  );
+}
+
+export default App;
